c78aed7a52cb652e2f43c52d12eb2a617dba08b1
Aina <ainaard@mail.ru>
2020-02-26 23:10:05 +0300

    fixed dtos, edited application properties file

--- src/main/java/ru/itis/dto/ItemDto.java
+++ src/main/java/ru/itis/dto/ItemDto.java
@@ -1,15 +1,11 @@
 package ru.itis.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.itis.models.Item;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class ItemDto {
     private String name;
     private int price;
