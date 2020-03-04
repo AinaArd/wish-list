17ee24daf332c2ac4514d5e42c6beda15ca80ca9
Aina <ainaard@mail.ru>
2020-03-04 19:43:36 +0300

    Merge branch 'jwt_tokens' of C:\AinaArd\wish-list with conflicts.

---
+++ src/main/java/ru/itis/dto/ResponseDto.java
@@ -0,0 +1,14 @@
+package ru.itis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto {
+    private HttpStatus status;
+    private Object data;
+}
